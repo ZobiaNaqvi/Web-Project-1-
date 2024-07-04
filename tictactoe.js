fdb6e3a01ade6e4130588e06a3db58a9dffccb7b
ZobiaNaqvi <zobia.fatima.naqvi@gmail.com>
2024-07-04 12:50:07 +0500

    Making clicking functionality in with all the boxes
    Tick marks appearing in all the boxes upon clicking. No repeats inside or outside the box.

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,32 @@
-const boxx = document.getElementById("boxx");
+// const boxx = document.getElementById("box");
 
-boxx.addEventListener("click", function() {
-  
-  const image = document.createElement("img");
+boxeslist = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+playerturn = 1;
 
-  // Set the image source and other attributes (optional)
-  image.src = "tic.png"; // Replace with the actual path to your image
-  image.alt = "Tic Tac Toe Image"; // Add an alternative text for accessibility
-  image.classList.add("tic-image"); // Add a CSS class for styling (optional)
+const boxes = document.querySelectorAll(".box");
+console.log(boxes);
 
-  // Append the image to the box
-  boxx.appendChild(image);
+boxes.forEach((eachbox) => {
+  eachbox.addEventListener("click", function () {
+    if (boxeslist[eachbox.id - 1] == 0) {
+      const image = document.createElement("img");
+      image.src = "tic.png";
+      image.alt = "tictactoe";
+      eachbox.appendChild(image);
+      boxeslist[eachbox.id - 1] = playerturn;
+      console.log(boxeslist);
+    }
+  });
 });
 
+// boxes.addEventListener("click", function() {
+
+//   const image = document.createElement("img");
+
+//   // Set the image source and other attributes (optional)
+//   image.src = "tic.png"; // Replace with the actual path to your image
+//   image.alt = "Tic Tac Toe Image"; // Add an alternative text for accessibility
+
+//   // Append the image to the box
+//   boxx.appendChild(image);
+// });
